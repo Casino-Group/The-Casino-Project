bf1bfc177813502ad4d255a7192710ce0bba6d5a
kristijanlazarov <kikolazarov8@gmail.com>
2023-05-25 23:33:00 +0200

    Version 2

--- slot_machine.py
+++ slot_machine.py
@@ -1,57 +1,95 @@
 import random
+import tkinter as tk
 
+# Define the symbols to use in the game
 symbols = ["🍒", "🍋", "🍉"]
 
-while True:
-    print("$Welcome to slots$\n")
-    tokens = int(input("Enter Tokens: "))
-    while tokens > 0:
-        print(f"You have ", tokens, " tokens \n")
-        try:
-            action = input("Enter '1' to spin or '0' to cash out: ")
-            if action == '0':
-                print("Thanks for playing!\n")
-                print("Your cash out is", tokens)
-                break
-            elif action == '1':
-                bet = int(input("Bet amount: "))
-            else:
-                print("Invalid input! Please enter '1' to spin or '0' to cash out.")
-                continue
 
-        except ValueError:
-            print("Please enter a whole number of tokens!")
-            continue
+# Define the function to play the game
+def play_game():
+    global tokens
+    # Get the bet amount from the user and validate the input
+    try:
+        bet = int(bet_entry.get())
+    except ValueError:
+        results_label.config(text="Please enter a valid bet amount.")
+        return
+    if bet <= 0:
+        results_label.config(text="Please enter a positive bet amount.")
+        return
+    if bet > tokens:
+        results_label.config(text="Not enough tokens.")
+        return
 
-        if bet > tokens:
-            print("Not enough tokens.")
-        else:
-            tokens -= bet
-            sq_one = random.choice(symbols)
-            sq_two = random.choice(symbols)
-            sq_three = random.choice(symbols)
+    # Deduct the bet amount from the user's tokens
+    tokens -= bet
+    tokens_label.config(text="Tokens: {}".format(tokens))
 
-            print()
+    # Spin the slots
+    rows = []
+    for _ in range(3):
+        row = [random.choice(symbols) for _ in range(3)]
+        rows.append(row)
 
-            print("|", random.choice(symbols), "|", random.choice(symbols), "|", random.choice(symbols), "|")
-            print("----------------")
+    # Display the results to the user
+    slots_label.config(
+        text="|{}|{}|{}|\n|{}|{}|{}|\n|{}|{}|{}|".format(rows[0][0], rows[0][1], rows[0][2], rows[1][0], rows[1][1],
+                                                         rows[1][2], rows[2][0], rows[2][1], rows[2][2]))
 
-            print("|", sq_one, "|", sq_two, "|", sq_three, "|")
-            print("----------------")
+    # Calculate the winnings
+    if rows[1][0] == rows[1][1] == rows[1][2]:
+        amount_won = bet * 5
+        tokens += amount_won
+        results_label.config(text="You won {} tokens!".format(amount_won))
+    else:
+        results_label.config(text="You lost this time.")
 
-            print("|", random.choice(symbols), "|", random.choice(symbols), "|", random.choice(symbols), "|")
-            print("----------------")
+    # Update the tokens label
+    tokens_label.config(text="Tokens: {}".format(tokens))
 
-            if sq_one == sq_two and sq_two == sq_three:
-                amount_won = bet * 2
-                print("You won", amount_won, "tokens!")
-                tokens += amount_won
-            else:
-                print("You lost this time")
 
-    if tokens == 0:
-        print("\nYou are out of tokens.")
-        print("Thanks for playing!")
-        print("")
-        print()
-    break
+# Define the function to check the token balance and exit the game
+def checkout():
+    global tokens
+    results_label.config(text="You have {} tokens.".format(tokens))
+    root.destroy()
+
+
+# Create the main window
+root = tk.Tk()
+root.title("Slots Game")
+
+# Create the widgets for the GUI
+start_label = tk.Label(root, text="Enter the number of tokens to start with:")
+start_entry = tk.Entry(root)
+start_button = tk.Button(root, text="Start", command=lambda: start_game(int(start_entry.get())))
+tokens_label = tk.Label(root, text="")
+bet_label = tk.Label(root, text="Bet amount:")
+bet_entry = tk.Entry(root)
+play_button = tk.Button(root, text="Play", command=play_game)
+slots_label = tk.Label(root, text="| | | |\n| | | |\n| | | |")
+results_label = tk.Label(root, text="")
+checkout_button = tk.Button(root, text="Checkout", command=checkout)
+
+# Add the widgets to the window
+start_label.pack()
+start_entry.pack()
+start_button.pack()
+tokens_label.pack()
+bet_label.pack()
+bet_entry.pack()
+play_button.pack()
+slots_label.pack()
+results_label.pack()
+checkout_button.pack()
+
+
+# Define the function to start the game
+def start_game(start_tokens):
+    global tokens
+    tokens = start_tokens
+    tokens_label.config(text="Tokens: {}".format(tokens))
+
+
+# Start the game loop
+root.mainloop()
